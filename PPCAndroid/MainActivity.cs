0027ba0e4974ca947438fcae028a77ef28547f7d
Kamil Rusin <kamilrusin19@gmail.com>
2019-07-08 13:34:52 +0200

    [WIP] Added new activity

--- PPCAndroid/MainActivity.cs
+++ PPCAndroid/MainActivity.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reactive.Disposables;
 using Android.App;
 using Android.OS;
-using Android.Support.V7.App;
-using Android.Support.Design.Widget;
-using Android.Util;
 using Android.Widget;
 using PPCAndroid.Shared.Service;
 using ReactiveUI;
@@ -23,23 +20,6 @@ namespace PPCAndroid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             OnCreateBase(savedInstanceState);
-            
-            /*var bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
-            bottomNavigation.NavigationItemSelected += (s, e) =>
-            {
-                switch (e.Item.ItemId)
-                {
-                    case Resource.Id.action_showpersonaldata:
-                        Toast.MakeText(this, "Action ShowPersonalData clicked", ToastLength.Short).Show();
-                        break;
-                    case Resource.Id.action_backtodashboard:
-                        Toast.MakeText(this, "Action BackToDashboard clicked", ToastLength.Short).Show();
-                        break;
-                    case Resource.Id.action_logout:
-                        Toast.MakeText(this, "Action Logout clicked", ToastLength.Short).Show();
-                        break;
-                }
-            };*/
         }
 
         protected override void BindCommands(CompositeDisposable disposables)
