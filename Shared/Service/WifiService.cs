ea87169172b5fa8f56ccfad3c381363399206f93
Kamil Rusin <kamilrusin19@gmail.com>
2019-07-09 11:08:14 +0200

    Added structure for wifi tasks

---
+++ Shared/Service/WifiService.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+
+namespace PPCAndroid.Shared.Service
+{
+    public class WifiService: IWifi
+    {
+        public WifiService()
+        {
+            
+        }
+
+
+        public Task Search()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
